cb995cc5804cf58ec68652e310012a1b7252c813
Vinayak2457 <sherlawarvinayak2@gmail.com>
2026-07-12 16:42:28 +0530

    Time: 1 ms (99.90%) | Memory: 56 MB (6.61%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+       int count=0;
+        int candidate=0;
+        for(int n:nums){
+            if(count==0){
+                candidate=n;
+            }
+            if(candidate==n) count++;
+            else count--;
+        }
+        return candidate;
+    }
+}
